07c257abff63da21f124d0f2f78f8fce1532d64b
Nikola Nikushev <nikola.nikushev@smartly.io>
2025-10-08 15:10:12 +0300

    Uee left join as shown on the demo for the users with books request

--- src/repository/UsersRepository.ts
+++ src/repository/UsersRepository.ts
@@ -7,8 +7,8 @@ export class UsersRepository extends Repository<User> {
 
     public usersWithBooks(): Promise<(User & { books: Book[] })[]> {
         return this.qb
-            .join("users_books", "users.id", "users_books.user_id")
-            .join("books", "users_books.book_id", "books.id")
+            .leftJoin("users_books", "users.id", "users_books.user_id")
+            .leftJoin("books", "users_books.book_id", "books.id")
             .select("users.*", this.db.raw("json_agg(books.*) as books"))
             .groupBy("users.id")
             .orderBy("users.id", "asc")
